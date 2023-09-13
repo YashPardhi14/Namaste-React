8e7cdbbba8a5eaf323842587314a2fff9ed94651
YashPardhi14 <yapardhi@gmail.com>
2023-09-13 16:41:37 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react"
+import ReactDOM from 'react-dom/client'
+
 /*
 <div id="parent">
     <div id="child">
@@ -14,7 +17,7 @@
 */
 
 const parent=React.createElement("div",{id:"parent"},[
-React.createElement("div",{id:"child1"},[React.createElement("h1",{},"I am H1 tag"),React.createElement("h2",{},"I am h2 tag")]
+React.createElement("div",{id:"child1"},[React.createElement("h1",{},"I am Parcel"),React.createElement("h2",{},"I am the Beast")]
 ),React.createElement("div",{id:"child2"},[React.createElement("h1",{},"I am H1 tag"),React.createElement("h2",{},"I am h2 tag")]
 )])
 // const heading = React.createElement("h1",{id:"heading",xyz:"class"},"Hello world from React!");
